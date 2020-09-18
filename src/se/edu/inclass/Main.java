7426c012f9baad330f3c3e2ec57353b83350ce94
randynyl <56857427+randynyl@users.noreply.github.com>
2020-09-18 17:05:48 +0800

    Add printDataUsingStreams method

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -21,6 +21,12 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+//        printDataUsingStreams(tasksData);
+
+        printDeadlinesUsingStreams(tasksData);
+
+        System.out.println("Total number of deadlines using streams: " + countDeadlinesUsingStreams(tasksData));
+
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -47,6 +53,15 @@ public class Main {
             System.out.println(t);
         }
     }
+
+    public static void printDataUsingStreams(ArrayList<Task> tasksData) {
+        System.out.println("Printing data usign streams");
+        tasksData.stream()
+                .forEach(System.out::println);
+
+    }
+
+
 
     public static void printDeadlines(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
